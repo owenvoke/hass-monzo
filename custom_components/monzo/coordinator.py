6cc509dd96ed2e31d7cdd81e3509d2ca74ea4287
Owen Voke <development@voke.dev>
2023-07-15 09:50:22 +0100

    chore: update to refresh token

--- custom_components/monzo/coordinator.py
+++ custom_components/monzo/coordinator.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 
 import async_timeout
@@ -22,10 +24,12 @@ class MonzoUpdateCoordinator(DataUpdateCoordinator):
         session: OAuth2Session,
     ) -> None:
         """Initialize the UpdateCoordinator for Monzo sensors."""
+        self._monzo: Monzo | None = None
+        self._account_id: str | None = None
         self._session: OAuth2Session = session
         self._entry: ConfigEntry = entry
 
-        self._monzo: Monzo = Monzo(access_token=session.token["access_token"])
+        self.refresh_monzo_instance()
 
         super().__init__(
             hass,
@@ -34,12 +38,34 @@ class MonzoUpdateCoordinator(DataUpdateCoordinator):
             update_interval=DEFAULT_UPDATE_INTERVAL,
         )
 
+    def refresh_monzo_instance(self) -> None:
+        self._monzo: Monzo = Monzo(access_token=self._session.token["access_token"])
+
     async def _async_update_data(self):
         async with async_timeout.timeout(5):
-            account = await self.hass.async_add_executor_job(
-                lambda: self._monzo.get_first_account()
+            token = self._session.token["access_token"]
+
+            await self.hass.async_add_executor_job(
+                lambda: self._session.async_ensure_token_valid()
             )
 
+            if token != self._session.token["access_token"]:
+                self.refresh_monzo_instance()
+
+            if self._account_id is None:
+                account = await self.hass.async_add_executor_job(
+                    lambda: self._monzo.get_first_account()
+                )
+
+                if not account:
+                    raise MonzoAccountError('No accounts could not be found.')
+
+                self._account_id = account["id"]
+
             return await self.hass.async_add_executor_job(
-                lambda: self._monzo.get_balance(account["id"])
+                lambda: self._monzo.get_balance(self._account_id)
             )
+
+
+class MonzoAccountError(Exception):
+    pass
